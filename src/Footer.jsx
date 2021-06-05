3a99adc130d3defa2e0a0ba608a960bdb2b63f1f
MdTasib <ohidulalam442@gmail.com>
2021-06-05 10:29:23 +0600

    project done

---
+++ src/Footer.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable jsx-a11y/accessible-emoji */
+/* eslint-disable react/jsx-no-target-blank */
+import React from 'react'
+
+function Footer() {
+    return (
+        <div style={{ textAlign: "center", color: "white" }}>
+            Made With ❤️ by Mohammad Tasib  | <a target="_blank" href="https://www.linkedin.com/in/mohammad-tasib-4341b71b4/">Contact Developer</a>
+        </div>
+    )
+}
+
+export default Footer;
